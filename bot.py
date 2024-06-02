f732f828a2457c5b4ca820fa51d6c0e0873aedc0
andyphancode <109082527+andyphancode@users.noreply.github.com>
2024-06-01 19:48:01 -0700

    created commands command and updated fix command

--- bot.py
+++ bot.py
@@ -25,14 +25,14 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('This is JH bot at your service, ready to figure out whatever your friend typed.')
+async def commands(ctx):
+    await ctx.send("""This is JH bot at your service, ready to figure out whatever your friend typed.\nHere are my available commands:\n```!commands: Get a list of commands\n\n!fix: Use this command and follow it up with any text to autocorrect it. For example sending [!fix helol eveyrone] without the brackets would result in me sending a message with the autocorrected response: hello everyone\n\n!re: Reply to a message with this command to autocorrect the message you replied to. For example, replying to a message that says [helol eveyrone] with !re would result in me sending a message with the autocorrected response: hello everyone\n\n!secret: Reply to a message with this command and see what happens! ```""")
 
 @bot.command()
 async def fix(ctx, *, sentence: str):
     # Correct the sentence using JamSpell
     corrected_sentence = corrector.FixFragment(sentence)
-    await ctx.send(corrected_sentence)
+    await ctx.send(f"The message you are correcting is:\n ```{sentence}```\n This is what I think it is:\n```{corrected_sentence}```")
 
 @bot.command()
 async def re(ctx):
