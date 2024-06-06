7cc4fcd10cafb6daf43a5668bf92f8975e8704bd
andyphancode <andyphanpxh@gmail.com>
2024-06-05 19:47:04 -0700

    created command update for bot

--- register_commands.py
+++ register_commands.py
@@ -1,9 +1,22 @@
 import requests
-import pyyaml
+import yaml
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
 
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 APPLICATION_ID = os.getenv('DISCORD_APPLICATION_ID')
+URL = f"https://discord.com/api/v9/applications/{APPLICATION_ID}/commands"
+
+with open("discord_commands.yaml", "r") as file:
+    yaml_content = file.read()
+
+commands = yaml.safe_load(yaml_content)
+headers = {"Authorization": f"Bot {TOKEN}", "Content-Type": "application/json"}
+
+# Send the POST request for each command
+for command in commands:
+    response = requests.post(URL, json=command, headers=headers)
+    command_name = command["name"]
+    print(f"Command {command_name} created: {response.status_code}")
