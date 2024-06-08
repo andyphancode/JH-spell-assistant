8bfdd66a4a60cd2141d72ead27c369267817ceab
andyphancode <andyphanpxh@gmail.com>
2024-06-08 04:17:34 -0700

    renovate commands + error fixing
    - changed jamspell to pyspellchecker due to AWS setup difficulties
    - re command removed
    - secret changed to fix variation
    - bottom coded added to help localhost testing

--- jh-spell-bot/src/app/main.py
+++ jh-spell-bot/src/app/main.py
@@ -1,31 +1,28 @@
 import os
-from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+from spellchecker import SpellChecker
 from mangum import Mangum
 from asgiref.wsgi import WsgiToAsgi
-import jamspell
+
 from secret import to_ye_olde_english
 from discord_interactions import verify_key_decorator
 
-load_dotenv()
-DISCORD_PUBLIC_KEY = os.environ.get('DISCORD_PUBLIC_KEY')
+DISCORD_PUBLIC_KEY = 'fd9aee8eaf1001ca5b335654aaf52ba5f66903cd9844d79bd95764410a161ebe'
 
 app = Flask(__name__)
 asgi_app = WsgiToAsgi(app)
 handler = Mangum(asgi_app)
-# Initialize jamspell
-corrector = jamspell.TSpellCorrector()
-corrector.LoadLangModel('en.bin')
+# Initialize SpellChecker instance
+spell = SpellChecker()
 
 
-@verify_key_decorator(DISCORD_PUBLIC_KEY)
 @app.route("/", methods=["POST"])
 async def interactions():
     print(f"👉 Request: {request.json}")
     raw_request = request.json
     return interact(raw_request)
 
-
+@verify_key_decorator(DISCORD_PUBLIC_KEY)
 def interact(raw_request):
     if raw_request["type"] == 1:  # PING
         response_data = {"type": 1}  # PONG
@@ -34,26 +31,49 @@ def interact(raw_request):
         command_name = data["name"]
 
         if command_name == "commands":
-            message_content = """This is JH bot at your service, ready to figure out whatever your friend typed.\nHere are my available commands:\n```!commands: Get a list of commands\n\n!fix: Use this command and follow it up with any text to autocorrect it. For example sending [!fix helol eveyrone] without the brackets would result in me sending a message with the autocorrected response: hello everyone\n\n!re: Reply to a message with this command to autocorrect the message you replied to. For example, replying to a message that says [helol eveyrone] with !re would result in me sending a message with the autocorrected response: hello everyone\n\n!secret: Reply to a message with this command and see what happens! ```"""
+            # content including reply and old /secret
+            # message_content = """This is JH bot at your service, ready to figure out whatever your friend typed.\nHere are my available commands:\n```!commands: Get a list of commands\n\n!fix: Use this command and follow it up with any text to autocorrect it. For example sending [!fix helol eveyrone] without the brackets would result in me sending a message with the autocorrected response: hello everyone\n\n!re: Reply to a message with this command to autocorrect the message you replied to. For example, replying to a message that says [helol eveyrone] with !re would result in me sending a message with the autocorrected response: hello everyone\n\n!secret: Reply to a message with this command and see what happens! ```"""
+            message_content = """This is JH bot at your service, ready to figure out whatever your friend typed.\nHere are my available commands:\n```!commands: Get a list of commands\n\n!fix: Use this command and follow it up with any text to autocorrect it. For example sending [!fix helol eveyrone] without the brackets would result in me sending a message with the autocorrected response: hello everyone\n\n!secret: Input a message with this command and see what happens! ```"""        
         elif command_name == "fix":
             original_message = data["options"][0]["value"]
-            corrected_message = corrector.FixFragment(original_message)
-            message_content = f"The message you are correcting is:\n ```{original_message}```\n This is what I think it is:\n```{corrected_message}```"
-        elif command_name == "re":
-            if "referenced_message" not in raw_request["message"] or raw_request["message"]["referenced_message"] is None:
-                message_content = "Please reply to a message you want to correct. This command only works with replies."
+            words = original_message.split()
+
+            # Correct words that can be corrected, leave the rest unchanged
+            corrected_words = [spell.correction(word) if spell.correction(word) is not None else word for word in words]
+
+            # Join the corrected words back into a sentence
+            corrected_message = " ".join(corrected_words)
+            if corrected_message is None:
+                message_content = "The typos in this message are beyond salvation..."
             else:
-                original_message = raw_request["message"]["referenced_message"]["content"]
-                corrected_message = corrector.FixFragment(original_message)
-                message_content = f"The user you are correcting sent:\n```{original_message}```\nThis is what I think it means:\n```{corrected_message}```"
+                message_content = f"The message you are correcting is:\n ```{original_message}```\n This is what I think it is:\n```{corrected_message}```"
+        # Made without realizing that slash commands cannot interact with a replied message, commented out for now but may have functionality in the future
+        # elif command_name == "re":
+        #     referenced_message = data.get("referenced_message")
+        #     if referenced_message is None:
+        #         print(f"👉 Data: {data}")
+        #         message_content = "Please reply to a message you want to correct. This command only works with replies."
+        #     else:
+        #         original_message = raw_request["message"]["referenced_message"]["content"]
+        #         words = original_message.split()
+        #         corrected_words = [spell.correction(word) for word in words]
+        #         corrected_message = " ".join(corrected_words)
+        #         message_content = f"The user you are correcting sent:\n```{original_message}```\nThis is what I think it means:\n```{corrected_message}```"
         elif command_name == "secret":
-            if "referenced_message" not in raw_request["message"] or raw_request["message"]["referenced_message"] is None:
-                message_content = "Please reply to a message you want to correct. This command only works with replies."
+            original_message = data["options"][0]["value"]
+            words = original_message.split()
+
+            # Correct words that can be corrected, leave the rest unchanged
+            corrected_words = [spell.correction(word) if spell.correction(word) is not None else word for word in words]
+
+            # Join the corrected words back into a sentence
+            corrected_message = " ".join(corrected_words)   
+            if corrected_message is None:
+                message_content = "The typos in this message are beyond salvation..."
             else:
-                original_message = raw_request["message"]["referenced_message"]["content"]
-                corrected_message = corrector.FixFragment(original_message)
                 giga_corrected_message = to_ye_olde_english(corrected_message)
-                message_content = f"The user you are correcting sent:\n```{original_message}```\nThis is what I think it means:\n```{giga_corrected_message}```"
+                message_content = f"The message you are correcting is:\n ```{original_message}```\n This is what I think it is:\n```{giga_corrected_message}```"
+
 
 
 
@@ -63,3 +83,6 @@ def interact(raw_request):
         }
 
     return jsonify(response_data)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
