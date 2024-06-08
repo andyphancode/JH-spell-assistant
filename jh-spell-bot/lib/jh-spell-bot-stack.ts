f201a8506cb79bada8ff0d99d23afafdb34e6564
andyphancode <andyphanpxh@gmail.com>
2024-06-08 04:18:16 -0700

    Update jh-spell-bot-stack.ts

--- jh-spell-bot/lib/jh-spell-bot-stack.ts
+++ jh-spell-bot/lib/jh-spell-bot-stack.ts
@@ -1,16 +1,35 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class JhSpellBotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const dockerFunction = new lambda.DockerImageFunction(
+      this,
+      "DockerFunction",
+      {
+        code: lambda.DockerImageCode.fromImageAsset("./src"),
+        memorySize: 1024,
+        timeout: cdk.Duration.seconds(10),
+        architecture: lambda.Architecture.X86_64,
+        environment: {
+          DISCORD_PUBLIC_KEY: "fd9aee8eaf1001ca5b335654aaf52ba5f66903cd9844d79bd95764410a161ebe",
+        },
+      }
+    );
 
-    // The code that defines your stack goes here
+    const functionUrl = dockerFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'JhSpellBotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: functionUrl.url,
+    });
   }
 }
